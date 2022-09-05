4bf910559379f112576ea4201f987f6510dd1f76
JRBusiness <43971633+JRBusiness@users.noreply.github.com>
2022-09-04 21:25:34 -0400

    sa

--- train_data/convert.py
+++ train_data/convert.py
@@ -233,22 +233,28 @@ def converting_paddle_SER(data1, data2, write_file):
                 total_dependent.append(item)
                 item["linking"] = []
         if [big_tem["id"] == 61 for big_tem in new_annotation]:
-            for i, item in enumerate(total_mem_name):
-                item["linking"].extend([[61, 21 + 44 + a] for a in range(len(total_mem_name))])
-                item["id"] = 21 + 44 + i
-            for item in new_annotation:
-                if item["id"] == 61:
+            if len(total_mem_name) >= 1:
+                ind = 0
+                for item in new_annotation:
                     item["linking"].extend([[61, 21 + 44 + a] for a in range(len(total_mem_name))])
+                    item["id"] = 21 + 44 + ind
+                    ind += 1
+                for item in new_annotation:
+                    if item["id"] == 61:
+                        item["linking"].extend([[61, 21 + 44 + a] for a in range(len(total_mem_name))])
         else:
             for i, item in enumerate(total_mem_name):
                 item["linking"] = []
         if [big_tem["id"] == 12 for big_tem in new_annotation]:
-            for i, item in enumerate(total_dependent):
-                item["linking"].extend([[12, 32 + 18 + a] for a in range(len(total_dependent))])
-                item["id"] = 32 + 18 + i
-            for item in new_annotation:
-                if item["id"] == 12:
+            if len(total_dependent) >= 1:
+                ind = 0
+                for item in new_annotation:
                     item["linking"].extend([[12, 32 + 18 + a] for a in range(len(total_dependent))])
+                    item["id"] = 32 + 18 + ind
+                    ind += 1
+                for item in new_annotation:
+                    if item["id"] == 12:
+                        item["linking"].extend([[12, 32 + 18 + a] for a in range(len(total_dependent))])
         else:
             for i, item in enumerate(total_dependent):
                 item["linking"] = []
